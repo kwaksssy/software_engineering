5ec8912fd358c975d7f0440f631c964b1b575acc
kwaksssy <kayalove19@gmail.com>
2025-06-05 16:03:32 +0900

    Update StudentManagerTest.java

--- software_engineering/src/students/StudentManagerTest.java
+++ software_engineering/src/students/StudentManagerTest.java
@@ -1,4 +1,4 @@
-package student;
+package students;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,40 +6,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class StudentManagerTest {
-	
-	StudentManager sm;
 
 	@BeforeEach
 	void setUp() throws Exception {
-		sm = new StudentManager();
-		sm.addStudent("곽성렬");
 	}
 
 	@Test
 	void testAddStudent() {
-		sm.addStudent("홍길동");
-		assertTrue(sm.hasStudent("홍길동"));
+		
 	}
 
 	@Test
 	void testRemoveStudent() {
-		sm.removeStudent("곽성렬");
-		assertFalse(sm.hasStudent("곽성렬"));
 	}
 
 	@Test
-	void testAddStudentException() {
-		assertThrows(IllegalArgumentException.class, () -> {
-			sm.addStudent("곽성렬");
-		});
+	void testHasStudent() {
 	}
-
-	@Test
-	void testRemoveStudentException() {
-		assertThrows(IllegalArgumentException.class, () -> {
-			sm.removeStudent("홍길동");
-		});
-	}
-//TEST
 
 }
